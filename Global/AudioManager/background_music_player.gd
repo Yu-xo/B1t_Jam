cb7ddcf62b867d7870f20f8124f98a8a63e5eae4
TTBob <erik.k.lagerstedt@gmail.com>
2025-12-04 19:35:20 +0100

    remove debug feature

--- Global/AudioManager/background_music_player.gd
+++ Global/AudioManager/background_music_player.gd
@@ -23,8 +23,6 @@ func _process(_delta: float) -> void:
 	var duration = get_playback_position() + AudioServer.get_time_since_last_mix()
 	if (stream.get_length() - duration < menu_start + (stream.get_length() - menu_end)):
 		play()
-	if (Input.is_action_just_pressed("down")):
-		_change_music(menu_music)
 
 func _change_music(_new_stream: AudioStream):
 	self.create_tween().tween_property(self, "volume_linear", 0, 0.5).finished.connect(func():
